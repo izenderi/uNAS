6937040e6fc796017e3eefae3a80d26cf36b085a
izenderi <ziliang1121@gmail.com>
2022-10-10 11:31:56 -0700

    r10-1.0.4 write out sav

--- search_algorithms/aging_evolution.py
+++ search_algorithms/aging_evolution.py
@@ -52,10 +52,17 @@ class GPUTrainer:
                              inference_latency(rg, compute_weight=1, mem_access_weight=0)]
         log.info(f"Training complete: val_error={val_error:.4f}, test_error={test_error:.4f}, "
                  f"resource_features={resource_features}.")
+        
+        model_file = self.root_dir / f"{self.experiment_name}_agingevosearch_state_final.sav"
+        self.save_model(model, model_file.as_posix())
+        
         return EvaluatedPoint(point=point,
                               val_error=val_error, test_error=test_error,
                               resource_features=resource_features)
 
+    def save_model(self, model, file):
+        with open(file, "wb") as f:
+            pickle.dump(model, f)
 
 class AgingEvoSearch:
     def __init__(self,
